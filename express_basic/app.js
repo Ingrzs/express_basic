479cb2ea92b23f5f4166919fa58e8611e3ed902a
Ingrzs <ing.ramirozs@gmail.com>
2022-04-24 13:38:11 -0500

    agregando ruta launchx al path

--- express_basic/app.js
+++ express_basic/app.js
@@ -4,9 +4,14 @@ const app = express()   //los datos de exprees los pasamos a app "lo convertimos
 
 const port = 3000  //el puerto que usaremos para express
 
-app.get('/',(req,res)=>{   //path que respondera la url localhost
+app.get('/',(req,res)=>{   //path que respondera la url localhost la diagonal indica localhost
     res.send('Hello world')
 })
+
+app.get('/launchx',(req,res)=>{   //path que respondera la url localhost/launchx
+    res.send('Bienvenido a launch')
+})
+
 
 app.listen(port,()=>{          //inicializamos la app express  
     console.log(`example en port ${port}`)
